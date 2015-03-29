9792f78e028dcf5e1af7978e8102c34e5e7dbdf4
mazenovi <vmazenod@gmail.com>
2015-03-29 22:45:20 +0200

    atmoscraper format enhancement + screenshot

--- src/scraper/atmoscraper.js
+++ src/scraper/atmoscraper.js
@@ -6,10 +6,12 @@
  *
  * Usage:
  *
- * $ bin/atmoscraper 014 --stream --format=csv
+ * $ bin/atmoscraper 014
  *
  * (all arguments will be used as the query)
  */
+
+var allMeasures = [];
 
 var casper = require("casper").create({
     waitTimeout: 1000,
@@ -17,8 +19,6 @@ var casper = require("casper").create({
         userAgent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/34.0.1847.116 Chrome/34.0.1847.116 Safari/537.36"
     }
 });
-
-var datas = [];
 
 var url = "http://www.atmoauvergne.asso.fr/fr/mesures/mesures-automatiques-par-station";
 
@@ -33,7 +33,6 @@ var station = casper.cli.args.join(" ");
 var help = casper.cli.options.about;
 var limit = casper.cli.options.limit || 10;
 var stream = casper.cli.options.stream;
-var format = casper.cli.options.format;
 var screenshot = casper.cli.options.screenshot;
 var wait = isNumber(casper.cli.options.wait) ? casper.cli.options.wait : 1;
 
@@ -65,29 +64,21 @@ function isNumber(n) {
 
 function usage() {
     casper
-        .echo("Return data from atmo station formated in JSON or CSV.")
+        .echo("Return data from selected atmo station formated in JSON.")
         .echo("")
         .echo("  Usage:")
         .echo("       $ casperjs atmoscraper.js 14")
-        .echo("       $ casperjs atmoscraper.js 16 --limit=5 --stream")
+        .echo("       $ casperjs atmoscraper.js 16 --limit=5")
         .echo("")
         .echo("  Options:")
         .echo("    --about                   show this help.")
-        .echo("    --station                 atmon station id.")
         .echo("    --limit=LIMIT             crawl LIMIT atmo auvergne pages (default 10).")
-        .echo("    --stream                  return results when available. This writes formated results as soon as it is extracted.")
-        .echo("    --format=FORMAT      format results in FORMAT. Currently FORMAT can be JSON or CSV (default csv)")
+        .echo("    --stream        return results when available. This writes formated results as soon as it is extracted.")
         .echo("    --screenshot    directory where to store screenshots")
         .echo("    --wait                    time to wait before parsing google results.")
         .echo("")
         .exit(1)
     ;
-}
-// transpose array - http://stackoverflow.com/questions/17428587/transposing-a-2d-array-in-javascript
-function transpose(a) {
-    return Object.keys(a[0]).map(
-        function (c) { return a.map(function (r) { return r[c]; }); }
-    );
 }
 
 // sub a day to date - http://stackoverflow.com/questions/4413590/javascript-get-array-of-dates-between-2-dates
@@ -96,7 +87,6 @@ Date.prototype.subDays = function(days) {
     dat.setDate(dat.getDate() - days);
     return dat;
 }
-
 // fr slashed date  render - http://stackoverflow.com/questions/3066586/get-string-in-yyyymmdd-format-from-js-date-object
 Date.prototype.ddmmyyyySlashed = function() {
    var yyyy = this.getFullYear().toString();
@@ -104,13 +94,33 @@ Date.prototype.ddmmyyyySlashed = function() {
    var dd  = this.getDate().toString();
    return (dd[1]?dd:"0"+dd[0]) + '/' + (mm[1]?mm:"0"+mm[0]) + '/' + yyyy; // padding
 }
-
 // en slashed date  render - http://stackoverflow.com/questions/3066586/get-string-in-yyyymmdd-format-from-js-date-object
 Date.prototype.yyyymmddSlashed = function() {
    var yyyy = this.getFullYear().toString();
    var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
    var dd  = this.getDate().toString();
    return yyyy + '/' + (mm[1]?mm:"0"+mm[0]) + '/' + (dd[1]?dd:"0"+dd[0]); // padding
+}
+
+function serializeScreenshot(filename) {
+    return JSON.stringify({type: 'screenshot', filename: filename});
+}
+function formatScreenshot(filename) {
+    return casper.echo(serializeScreenshot(filename));
+}
+function takeScreenshot() {
+    var screenshotFile = (+new Date()).toString(36) + '.png';
+
+    casper
+        .capture(screenshot + '/' + screenshotFile)
+        .then(function () {
+            // send screenshot
+            if (stream) {
+                formatScreenshot(screenshotFile);
+            }
+        });
+
+    return casper;
 }
 
 // retrieve data's titles per column from current page
@@ -122,10 +132,9 @@ function getTableTitles() {
     });   
     return row;
 }
-
-// retrieve data per column from current page
-function getTableData() {
-    var data = [];
+// retrieve mesures per column from current page
+function getTableMeasures() {
+    var measures = [];
     trs = document.querySelectorAll("table.table-mesures-par-station tbody tr");
     [].forEach.call(trs, function(tr) {
     // ??? may I trs.forEach( function(tr) {
@@ -135,45 +144,28 @@ function getTableData() {
             // replace empty cell by ??
             row.push(td.textContent); 
         });
-        data.push(row);
+        measures.push(row);
     });   
-    return data;
+    return measures;
 }
-
-function serializeDate(day) {
-  return JSON.stringify({ type: 'day', day: day.ddmmyyyySlashed() });
+// transpose array - http://stackoverflow.com/questions/17428587/transposing-a-2d-array-in-javascript
+function transpose(a) {
+    return Object.keys(a[0]).map(
+        function (c) { return a.map(function (r) { return r[c]; }); }
+    );
 }
-
-function serializeUrl(url) {
-  return JSON.stringify({ type: 'url', url: url });
-}
-
-// standardize crawled data
-function standardizeData(titles, data) {
-  data[0].forEach(function(e) {
+// standardize crawled mesures
+function standardizeMeasures(titles, measures) {
+  measures[0].forEach(function(e) {
     e = currentDay.yyyymmddSlashed() + " " + e;
   });
   columnTitles.forEach(function(title, index) {
     if (titles.indexOf(title) == -1) {
-      data.splice(index, 0, new Array(data[0].length+1).join('0').split(''));
+      measures.splice(index, 0, new Array(measures[0].length+1).join('0').split(''));
     }
   });
-  return data;
+  return measures;
 }
-
-// format crawled data
-function serializeData(data) {
-  if (format == 'csv' || format == 'CSV') {
-    var csv = '';
-    [].forEach.call(data, function(line) {
-      csv += line.join(',') + "\n";
-    });
-    return csv;    
-  }
-  return JSON.stringify({ type: 'data', data: data });
-}
-
-
 
 // handle page crawling
 var processPage = function() {
@@ -182,21 +174,26 @@ var processPage = function() {
     this//.echo('Will wait for ' + Math.floor(waitTime))
         .wait(waitTime * 1000);
     
-    this.echo(serializeDate(currentDay));//, 'COMMENT');
-
     var titles = this.evaluate(getTableTitles); // ??? why evaluate
 
-    var data = transpose(this.evaluate(getTableData)); // ??? why evaluate
+    var measures = transpose(this.evaluate(getTableMeasures)); // ??? why evaluate
 
-    data = standardizeData(titles, data);
+    measures = standardizeMeasures(titles, measures);
 
-    data = transpose(data);
+    measures = transpose(measures);
+
+    if (screenshot) {
+      takeScreenshot();
+    }
+    
+    JSONifyMeasures = { type: 'measures', day: currentDay.ddmmyyyySlashed(), url: url, data: measures };
 
     if (stream) {
-      this.echo(serializeData(data));//, 'INFO');
+
+      this.echo(JSON.stringify(JSONifyMeasures));//, 'INFO');
     }
 
-    datas.unshift(data);
+    allMeasures = allMeasures.concat([JSONifyMeasures]);
 
     // don't go too far down the rabbit hole
     if (currentPage >= limit || this.exists('<h1>Mesures automatiques par station</h1>')) {
@@ -229,14 +226,16 @@ var getPage = function() {
       })
       .waitForSelector('.mesures-auvergne-resultats', processPage); // ??? terminate);
 
-  this.echo(serializeUrl(url));
 };
 
 // write links to the output if not streamed.
 function terminate(err){
+  if (screenshot) {
+    takeScreenshot();
+  }
   this.then(function () {
     if (!stream) {
-      this.echo(formatData(datas), 'INFO');
+      this.echo(JSON.stringify(allMeasures));
     }
   });
 }
